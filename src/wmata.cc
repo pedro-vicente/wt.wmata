e19016834c1012ae2419422808be96a45db8889a
pvn <pvn>
2025-11-06 21:38:22 -0500

    load map

--- src/wmata.cc
+++ src/wmata.cc
@@ -1,20 +1,371 @@
 #include <Wt/WApplication.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WCompositeWidget.h>
+#include <Wt/WHBoxLayout.h>
+#include <Wt/WText.h>
+#include <Wt/WVBoxLayout.h>
+#include <Wt/WCheckBox.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WBreak.h>
+#include <Wt/WApplication.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WCompositeWidget.h>
+#include <Wt/WText.h>
+#include <Wt/Json/Object.h>
+#include <Wt/Json/Parser.h>
+#include <Wt/Json/Array.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <vector>
+#include <string>
+#include <map>
+#include <sstream>
+#include <iomanip> 
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <map>
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// ApplicationWmata
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// load_geojson
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class ApplicationWmata : public Wt::WApplication
+std::string load_geojson(const std::string& name)
 {
-public:
-  ApplicationWmata(const Wt::WEnvironment& env);
+  std::ifstream file(name);
+  if (!file.is_open())
+  {
+    return "";
+  }
+  auto start_time = std::chrono::high_resolution_clock::now();
+  std::string str;
+  std::string line;
+  while (std::getline(file, line))
+  {
+    str += line;
+  }
+  file.close();
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+  return str;
+}
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//to_hex
+//convert int to hex string, apply zero padding
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+std::string to_hex(int n)
+{
+  std::stringstream ss;
+  ss << std::hex << std::setw(2) << std::setfill('0') << n; // Use std::setw and std::setfill for zero padding
+  return ss.str();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//rgb_to_hex
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+std::string rgb_to_hex(int r, int g, int b)
+{
+  std::string str("#");
+  str += to_hex(r);
+  str += to_hex(g);
+  str += to_hex(b);
+  return str;
+}
+
+std::vector<std::string> ward_color =
+{ rgb_to_hex(128, 128, 0), //olive
+  rgb_to_hex(255, 255, 0), //yellow 
+  rgb_to_hex(0, 128, 0), //green
+  rgb_to_hex(0, 255, 0), //lime
+  rgb_to_hex(0, 128, 128), //teal
+  rgb_to_hex(0, 255, 255), //aqua
+  rgb_to_hex(0, 0, 255), //blue
+  rgb_to_hex(128, 0, 128) //purple
 };
 
-ApplicationWmata::ApplicationWmata(const Wt::WEnvironment& env)
-  : WApplication(env)
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// WMapLibre
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Wt
 {
-  
+  class WT_API WMapLibre : public WCompositeWidget
+  {
+    class Impl;
+  public:
+    WMapLibre();
+    ~WMapLibre();
+
+    std::string geojson;
+
+  protected:
+    Impl* impl;
+    virtual void render(WFlags<RenderFlag> flags) override;
+  };
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Station
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct Station
+{
+  std::string code;
+  std::string name;
+  double lat;
+  double lon;
+  std::string lineCode;
+  std::string address;
+
+  Station(const std::string& c, const std::string& n, double lt, double ln,
+    const std::string& lc, const std::string& addr)
+    : code(c), name(n), lat(lt), lon(ln), lineCode(lc), address(addr) {
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// globals
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+std::string geojson_wards;
+std::vector<Station> stations;
+
+std::map<std::string, std::string> line_colors =
+{
+  {"RD", "#E51636"}, // Red Line
+  {"OR", "#F68712"}, // Orange Line
+  {"SV", "#9D9F9C"}, // Silver Line
+  {"BL", "#1574C4"}, // Blue Line
+  {"YL", "#FFD100"}, // Yellow Line
+  {"GR", "#0FAB4B"}  // Green Line
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// load_file
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+std::string load_file(const std::string& filename)
+{
+  std::ifstream file(filename);
+  if (!file.is_open())
+    return "";
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return buffer.str();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// parse_stations
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void parse_stations(const std::string& json_str)
+{
+  stations.clear();
+
+  try
+  {
+    Wt::Json::Object root;
+    Wt::Json::parse(json_str, root);
+
+    if (root.contains("Stations"))
+    {
+      const Wt::Json::Array& stations_ = root.get("Stations");
+
+      for (size_t idx = 0; idx < stations_.size(); ++idx)
+      {
+        const Wt::Json::Object& station = stations_[idx];
+
+        std::string code = station.get("Code").orIfNull("");
+        std::string name = station.get("Name").orIfNull("");
+        double lat = station.get("Lat").orIfNull(0.0);
+        double lon = station.get("Lon").orIfNull(0.0);
+        std::string lineCode = station.get("LineCode1").orIfNull("");
+
+        std::string address = "";
+        if (station.contains("Address"))
+        {
+          const Wt::Json::Object& addrObj = station.get("Address");
+          address = addrObj.get("Street").orIfNull("");
+        }
+
+        stations.emplace_back(code, name, lat, lon, lineCode, address);
+      }
+    }
+  }
+  catch (const std::exception& e)
+  {
+    Wt::log("") << e.what();
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// WMapLibre
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Wt
+{
+  class WMapLibre::Impl : public WWebWidget
+  {
+  public:
+    Impl();
+    virtual DomElementType domElementType() const override;
+  };
+
+  WMapLibre::Impl::Impl()
+  {
+    setInline(false);
+  }
+
+  DomElementType WMapLibre::Impl::domElementType() const
+  {
+    return DomElementType::DIV;
+  }
+
+  WMapLibre::WMapLibre()
+  {
+    setImplementation(std::unique_ptr<Impl>(impl = new Impl()));
+    WApplication* app = WApplication::instance();
+    this->addCssRule("body", "margin: 0; padding: 0;");
+    this->addCssRule("#" + id(), "position: absolute; top: 0; bottom: 0; width: 100%;");
+    app->useStyleSheet("https://unpkg.com/maplibre-gl@4.7.1/dist/maplibre-gl.css");
+    const std::string library = "https://unpkg.com/maplibre-gl@4.7.1/dist/maplibre-gl.js";
+    app->require(library, "maplibre");
+  }
+
+  WMapLibre::~WMapLibre()
+  {
+    geojson.clear();
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  // render
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  void WMapLibre::render(WFlags<RenderFlag> flags)
+  {
+    WCompositeWidget::render(flags);
+
+    if (flags.test(RenderFlag::Full))
+    {
+      std::stringstream js;
+
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+      // create map
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      js << "const map = new maplibregl.Map({\n"
+        << "  container: " << jsRef() << ",\n"
+        << "  style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',\n"
+        << "  center: [-77.0369, 38.9072],\n"
+        << "  zoom: 12\n"
+        << "});\n"
+
+        << "map.addControl(new maplibregl.NavigationControl());\n";
+
+#ifdef _WIN32
+      OutputDebugStringA(js.str().c_str());
+#endif
+
+      js << "map.on('load', function() {\n";
+
+      js << "var ward_color = [";
+      for (size_t idx = 0; idx < ward_color.size(); ++idx)
+      {
+        js << "'" << ward_color[idx] << "'";
+        if (idx < ward_color.size() - 1) js << ",";
+      }
+      js << "];\n";
+
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+      // add ward layer
+      /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      js << "map.addSource('wards', {\n"
+        << "  'type': 'geojson',\n"
+        << "  'data': " << geojson << "\n"
+        << "});\n"
+
+        << "map.addLayer({\n"
+        << "  'id': 'wards-fill',\n"
+        << "  'type': 'fill',\n"
+        << "  'source': 'wards',\n"
+        << "  'paint': {\n"
+        << "    'fill-color': ['get', ['to-string', ['get', 'WARD']], ['literal', {\n";
+
+      for (size_t idx = 0; idx < ward_color.size(); ++idx)
+      {
+        js << "      '" << (idx + 1) << "': '" << ward_color[idx] << "'";
+        if (idx < ward_color.size() - 1) js << ",\n";
+      }
+
+      js << "\n    }]],\n"
+        << "    'fill-opacity': 0.2\n"
+        << "  }\n"
+        << "});\n";
+
+#ifdef _WIN32
+      OutputDebugStringA(js.str().c_str());
+#endif
+
+      //close map.on('load')
+      js << "});\n";
+
+      WApplication* app = WApplication::instance();
+      app->doJavaScript(js.str());
+    }
+  }
+
+}// namespace Wt
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// ApplicationMap
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class ApplicationMap : public Wt::WApplication
+{
+public:
+  ApplicationMap(const Wt::WEnvironment& env);
+  virtual ~ApplicationMap();
+
+private:
+  Wt::WMapLibre* map;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// ApplicationMap
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ApplicationMap::ApplicationMap(const Wt::WEnvironment& env)
+  : WApplication(env), map(nullptr)
+{
+  map = root()->addWidget(std::make_unique<Wt::WMapLibre>());
+  map->resize(1920, 1080);
+
+  std::string stations_json = load_file("station_list.json");
+  if (!stations_json.empty())
+  {
+    parse_stations(stations_json);
+  }
+
+  if (!geojson_wards.empty())
+  {
+    map->geojson = geojson_wards;
+  }
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// ~ApplicationMap
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ApplicationMap::~ApplicationMap()
+{
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -23,7 +374,7 @@ ApplicationWmata::ApplicationWmata(const Wt::WEnvironment& env)
 
 std::unique_ptr<Wt::WApplication> create_application(const Wt::WEnvironment& env)
 {
-  return std::make_unique<ApplicationWmata>(env);
+  return std::make_unique<ApplicationMap>(env);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,5 +383,7 @@ std::unique_ptr<Wt::WApplication> create_application(const Wt::WEnvironment& env
 
 int main(int argc, char* argv[])
 {
-  return Wt::WRun(argc, argv, &create_application);
+  geojson_wards = load_geojson("ward-2012.geojson");
+  int result = Wt::WRun(argc, argv, &create_application);
+  return result;
 }
